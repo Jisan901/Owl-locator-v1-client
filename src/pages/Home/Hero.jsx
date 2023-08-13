f49a8a57f8e48875a04a3c7c9aff989ccb8f30f0
Jisan901 <jisuna901@gmail.com>
2023-08-13 18:45:50 +0600

    fix

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,8 +1,10 @@
+import heroImg from '../../assets/auntum.png'
+
 function Hero() {
     return (
         <div className="hero min-h-lg my-12 lg:my-16 max-w-3xl lg:max-w-5xl mx-auto bg-base-100">
             <div className="hero-content flex-col lg:flex-row-reverse text-center lg:text-left">
-                <img src="/src/assets/auntum.png" className="max-w-xs lg:max-w-sm rounded-lg shadow-2xl ml-0 mb-5 lg:ml-5 lg:mb-0"/>
+                <img src={heroImg} className="max-w-xs lg:max-w-sm rounded-lg shadow-2xl ml-0 mb-5 lg:ml-5 lg:mb-0"/>
                 <div>
                     <h1 className="text-5xl font-bold">Owl Locator!</h1>
                     <p className="py-6 max-w-md">
